85e49a03c8b576e4434ab7f653d58917f148916e
ipeterov <ipeterov1@gmail.com>
2023-01-14 21:58:32 +0400

    Improve responsive design

--- src/components/App.js
+++ src/components/App.js
@@ -6,12 +6,10 @@ import IconTable from "./IconTable";
 
 function App() {
   return (
-    <div className={styles.container}>
-      <div className={styles.tables}>
-        <IconTable name="Perks" icons={data.perks} columns={9}/>
-        <IconTable name="Spells" icons={data.spells} columns={12}/>
-        <IconTable name="Enemies" icons={data.enemies} columns={9}/>
-      </div>
+    <div className={styles.tables}>
+      <IconTable name="Perks" icons={data.perks} columns={9}/>
+      <IconTable name="Spells" icons={data.spells} columns={12}/>
+      <IconTable name="Enemies" icons={data.enemies} columns={9}/>
     </div>
   );
 }
