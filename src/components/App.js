f652d1e8bbabbbf64189bc033810e9150eb6c300
ipeterov <ipeterov1@gmail.com>
2023-01-15 14:46:15 +0400

    Add Hämis

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,18 @@
 import data from '../icons.json';
 
+import Hamis from '../img/longleg.png';
 import styles from './App.module.css';
 import IconTable from "./IconTable";
 
-
-function App() {
+export default function App() {
   return (
-    <div className={styles.tables}>
-      <IconTable name="Perks" icons={data.perks} columns={9}/>
-      <IconTable name="Spells" icons={data.spells} columns={12}/>
-      <IconTable name="Enemies" icons={data.enemies} columns={9}/>
+    <div className={styles.container}>
+      <div className={styles.tables}>
+        <IconTable name="Perks" icons={data.perks} columns={9}/>
+        <IconTable name="Spells" icons={data.spells} columns={12}/>
+        <IconTable name="Enemies" icons={data.enemies} columns={9}/>
+      </div>
+      <img src={Hamis} alt="Hämis" className={styles.hamis}/>
     </div>
   );
 }
-
-export default App;
